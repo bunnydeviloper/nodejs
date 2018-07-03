b2c1d6a51a32cf0808d2f3d5d911fc8338e0d7cc
Sophia Mai <phuongmai001@gmail.com>
2018-07-03 01:27:59 -0700

    add date and time module

---
+++ myfirstmodule.js
@@ -0,0 +1,6 @@
+// create your own module that returns the date and time
+// Use the exports keyword to make properties and methods available outside the module file.
+
+exports.myDateTime = function () {
+    return Date();
+};
