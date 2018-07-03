d2074acef3dcb86132e711321fda7dda1d6f6571
Sophia Mai <phuongmai001@gmail.com>
2018-07-03 01:58:52 -0700

    update date time module

--- 002-create-your-own-time-module/demo_module.js
+++ 002-create-your-own-time-module/demo_module.js
@@ -1,11 +1,8 @@
-// modules are the same as JS libraries (a set of fn(s) you want to include in your application)
-// built-in modules (no installation needed) and external modules
 const http = require('http');
-const dt = require('./myfirstmodule');
+const dt = require('./myfirstmodule'); // import your own module
 
-// create a server, when user access via http://localhost:8080/, they get back 'Hello World!'
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time are currently: " + dt.myDateTime());
-    res.end('Hello World!');
+    res.end();
 }).listen(8080);
