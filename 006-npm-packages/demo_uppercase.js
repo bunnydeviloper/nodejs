751324a78109092397cbbd87925971656f21749e
Sophia Mai <phuongmai001@gmail.com>
2018-07-03 12:54:02 -0700

    add demo uppercase file

---
+++ 006-npm-packages/demo_uppercase.js
@@ -0,0 +1,9 @@
+// NPM is a package manager for Node.js packages, or modules (~ JS libraries) if you like.
+// The NPM program is installed on your computer when you install Node.js
+
+// npm install upper-case --> downloaded and installed your first package!
+// NPM creates a folder named "node_modules", where the package will be placed.
+// All packages you install in the future will be placed in this folder.
+// Folder structure --> C:\Users\My Name\node_modules\upper-case
+
+
