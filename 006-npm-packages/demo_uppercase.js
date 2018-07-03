8d403c32a946290cfbefa53a89ec35ccc77173dc
Sophia Mai <phuongmai001@gmail.com>
2018-07-03 12:59:10 -0700

    update hello world upper case

--- 006-npm-packages/demo_uppercase.js
+++ 006-npm-packages/demo_uppercase.js
@@ -6,4 +6,13 @@
 // All packages you install in the future will be placed in this folder.
 // Folder structure --> C:\Users\My Name\node_modules\upper-case
 
+const uc = require('upper-case');
+const http = require('http');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-type": "text/html"});
+  res.write(uc("hello world!")); // this will print 'HELLO WORLD!' on screen
+  res.end();
+}).listen(8080);
+
 
